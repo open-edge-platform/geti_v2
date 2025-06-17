131c2aba05575fbb80d1dd7c527e9cb2eeee89b1
Joao Vilaca <joao.vilaca@intel.com>
2025-06-17 14:11:10 +0200

    Update index and core service imports

--- web_ui/packages/core/index.ts
+++ web_ui/packages/core/index.ts
@@ -2,6 +2,15 @@
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
 export { apiClient } from './src/client/axios-instance';
+
 export { paths } from './src/services/routes';
 export { API_URLS } from './src/services/urls';
 export { addHostToApiUrls } from './src/services/use-api-router.hook';
+
+export { useFeatureFlags } from './src/feature-flags/hooks/use-feature-flags.hook';
+export { createInMemoryApiFeatureFlagService } from './src/feature-flags/services/in-memory-api-feature-flag-service';
+export {
+    type CustomFeatureFlags,
+    type FeatureFlags,
+    DEV_FEATURE_FLAGS,
+} from './src/feature-flags/services/feature-flag-service.interface';
