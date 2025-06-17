9cfdcf57fa4c88f5ff9ef04c5baa125ce9c36e6d
Joao Vilaca <joao.vilaca@intel.com>
2025-06-17 14:10:56 +0200

    Move feature flag files

--- web_ui/packages/core/src/feature-flags/hooks/use-feature-flag-query.hook.ts
+++ web_ui/packages/core/src/feature-flags/hooks/use-feature-flag-query.hook.ts
@@ -1,12 +1,12 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import { useApplicationServices } from '@geti/core/src/services/application-services-provider.component';
 import { useSuspenseQuery, UseSuspenseQueryOptions, UseSuspenseQueryResult } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { isEmpty } from 'lodash-es';
 
-import QUERY_KEYS from '../../../../packages/core/src/requests/query-keys';
+import QUERY_KEYS from '../../requests/query-keys';
+import { useApplicationServices } from '../../services/application-services-provider.component';
 import { FeatureFlags, FeatureFlagService } from '../services/feature-flag-service.interface';
 
 const featureFlagQueryOptions = (
