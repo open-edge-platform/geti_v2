a2d6ac2f3c35a2b425669a8d42de1cd450abbc8b
Albert van Houten <albert.van.houten@intel.com>
2025-06-25 09:07:09 +0200

    Remove logic to periodically refresh logging config

--- libs/telemetry_tools/src/geti_logger_tools/logger_config.py
+++ libs/telemetry_tools/src/geti_logger_tools/logger_config.py
@@ -3,17 +3,13 @@ Configuration of base logger for the entire platform.
 Logger parameters are stored in a config map and are available in every container having it mounted.
 """
 
-import asyncio
-import json
 import logging
 import os
-import time
-from threading import Thread
-
-from geti_logger_tools.constants import DEFAULT_CONFIG_DIR, DEFAULT_CONFIG_FILE
 
 LOGGER_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 NON_CONFIGURABLE_LOGGERS = ["uvicorn.access", "werkzeug", "pika", "aiohttp.access", "geti_logger_tools.logger_config"]
+# TODO: https://jira.devtools.intel.com/browse/ITEP-70813
+LOG_LEVEL = logging._nameToLevel[os.getenv("LOG_LEVEL", "INFO")]
 
 
 class SanitizeLogFilter(logging.Filter):
@@ -34,59 +30,7 @@ def get_logging_format(extra_headers: str = "") -> str:
     return f"%(asctime)s,%(msecs)03d [%(levelname)-8s] [%(name)s:%(lineno)d]{extra_headers_str}: %(message)s"
 
 
-def _refresh(file_path, log_level=None):  # noqa: ANN001
-    """
-    Dynamically refreshes logger with information from given config file
-    :param file_path: location of config file for logger, defined in the config map
-    :param log_level: currently used log_level (taken from config map)
-    :return: new log level
-    """
-    try:
-        with open(file_path) as file:
-            logging_config = json.load(file)
-            new_level = logging_config["LOG_LEVEL"]
-    except FileNotFoundError:
-        return log_level
-
-    try:
-        if log_level != new_level:
-            for single_logger in logging.Logger.manager.loggerDict.copy():
-                logger = logging.getLogger(single_logger)
-                if logger.name not in NON_CONFIGURABLE_LOGGERS:
-                    logger.setLevel(getattr(logging, new_level))
-                    # Do not output first initialization
-                    if log_level is not None:
-                        logging.getLogger(__name__).info(
-                            f"Changed LOG_LEVEL to: {new_level} for logger name: {logger.name}"
-                        )
-
-    except AttributeError:
-        logging.getLogger(__name__).error(f"Wrong LOG_LEVEL value: {new_level} in given config file: {file_path}")
-        return log_level
-    return new_level
-
-
-def refresh_logger(file_path, log_level=None, interval=30, loop=None, use_async=True) -> None:  # noqa: ANN001
-    """
-    Dynamically refreshes logger with information from given config file
-    :param file_path: location of config file for logger, defined in the config map
-    :param log_level: currently used log_level (taken from config map)
-    :param interval: time of periodical refresh for the logger
-    :param loop: asynchronous loop to perform refresh
-    :param use_async: whether to use asyncio for scheduling refresh task (if not then threading)
-    """
-    if use_async:
-        if not loop:
-            loop = asyncio.get_event_loop()
-        new_level = _refresh(file_path, log_level)
-        loop.call_later(interval, refresh_logger, file_path, new_level, interval, loop, use_async)
-    else:
-        while True:
-            log_level = _refresh(file_path, log_level)
-            time.sleep(interval)
-
-
-def initialize_logger(package_name: str, use_async: bool = True, logging_format: str | None = None) -> logging.Logger:
+def initialize_logger(package_name: str, logging_format: str | None = None) -> logging.Logger:
     """
     Initialize logger and provide periodical dynamic refresh for its configuration.
     Adds a filter to sanitize log messages to prevent log injection.
@@ -98,23 +42,7 @@ def initialize_logger(package_name: str, use_async: bool = True, logging_format:
     """
     logger = logging.getLogger(package_name)
     logger.addFilter(SanitizeLogFilter())
-    logging_config_dir = os.getenv("LOGGING_CONFIG_DIR", DEFAULT_CONFIG_DIR)
-    config_filepath = f"{logging_config_dir}/{DEFAULT_CONFIG_FILE}"
     if logging_format is None:
         logging_format = get_logging_format()
     logging.basicConfig(level=logging.INFO, format=logging_format, datefmt=LOGGER_DATE_FORMAT, force=True)
-
-    if use_async:
-        loop = asyncio.get_event_loop()
-        loop.call_soon(refresh_logger, config_filepath)
-    else:
-        # Initial blocking refresh for setting first logging level before app start
-        _refresh(config_filepath)
-        refresh_thread = Thread(
-            target=refresh_logger,
-            args=(config_filepath,),
-            kwargs={"use_async": use_async},
-            daemon=True,
-        )
-        refresh_thread.start()
     return logger
--- libs/telemetry_tools/tests/unit/logging/test_unit_logging.py
+++ libs/telemetry_tools/tests/unit/logging/test_unit_logging.py
@@ -8,7 +8,7 @@ from geti_logger_tools.logger_config import initialize_logger
 
 class TestSanitizeLogFilter(unittest.TestCase):
     def test_sanitize_log_filter(self):
-        test_logger = initialize_logger(__name__, use_async=False)
+        test_logger = initialize_logger(__name__)
         with self.assertLogs(level="INFO", logger=test_logger) as log:
             test_logger.info("This is a test message\nwith a newline\rand a carriage return")
         self.assertIn("This is a test message with a newline and a carriage return", log.output[0])
