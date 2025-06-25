8fa2d92d1a7c3ec1558dc5f0fa6143f1723ec921
Colorado, Camilo <camilo.colorado@intel.com>
2025-06-25 10:53:10 +0200

    add unit tests

--- web_ui/src/test-utils/mocked-items-factory/mocked-keypoint.ts
+++ web_ui/src/test-utils/mocked-items-factory/mocked-keypoint.ts
@@ -2,6 +2,7 @@
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
 import { KeypointNode } from '../../core/annotations/shapes.interface';
+import { KeypointStructureDTO } from '../../core/projects/dtos/task.interface';
 import { getMockedLabel } from './mocked-labels';
 
 export const getMockedKeypointNode = (point?: Partial<KeypointNode>): KeypointNode => {
@@ -11,5 +12,16 @@ export const getMockedKeypointNode = (point?: Partial<KeypointNode>): KeypointNo
         isVisible: true,
         label: getMockedLabel({ color: '#000000' }),
         ...point,
+    };
+};
+
+export const getMockedKeypointStructureDto = (props?: Partial<KeypointStructureDTO>): KeypointStructureDTO => {
+    return {
+        edges: [{ nodes: ['label_1', 'label_2'] }],
+        positions: [
+            { label: 'label_1', x: 0.248, y: 0.25 },
+            { label: 'label_2', x: 0.33066666666666666, y: 0.506 },
+        ],
+        ...props,
     };
 };
