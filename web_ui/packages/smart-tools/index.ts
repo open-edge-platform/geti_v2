93eccfe05f9639aa001577fe7fd5b89b1eb13d49
Joao Vilaca <joao.vilaca@intel.com>
2025-06-25 10:26:08 +0200

    Fix rebase conflicts

--- web_ui/packages/smart-tools/index.ts
+++ web_ui/packages/smart-tools/index.ts
@@ -6,6 +6,8 @@ export { default as OpenCVLoader } from './src/utils/opencv-loader';
 export { Watershed } from './src/watershed/watershed';
 export { type WatershedInstance, type WatershedPolygon } from './src/watershed/interfaces';
 
+export { InferenceImage } from './src/inference-image/inference-image';
+
 export {
     formatContourToPoints,
     approximateShape,
