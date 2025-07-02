f104e5d57f9091c9e02c820a6d5a3fe2f8d3ea7a
Joao Vilaca <joao.vilaca@intel.com>
2025-07-02 13:58:45 +0200

    Remove unused interfaces

--- web_ui/packages/smart-tools/src/inference-image/interfaces.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2022-2025 Intel Corporation
-// LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
-
-import type OpenCVTypes from 'OpenCVTypes';
-
-export interface InferenceImage {
-    getImage(imageData: ImageData): OpenCVTypes.Mat;
-    resize: (imageData: ImageData, width: number, height: number) => ImageData;
-}
