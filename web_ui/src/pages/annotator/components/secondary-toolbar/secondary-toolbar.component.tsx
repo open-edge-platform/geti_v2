2db3232d31688cb2d20488cd2d37eff8aa6c484b
Joao Vilaca <joao.vilaca@intel.com>
2025-07-09 07:55:40 +0200

    Update submit button tooltip alignment

--- web_ui/src/pages/annotator/components/secondary-toolbar/secondary-toolbar.component.tsx
+++ web_ui/src/pages/annotator/components/secondary-toolbar/secondary-toolbar.component.tsx
@@ -145,7 +145,7 @@ export const SecondaryToolbar = ({ annotationToolContext }: ToolAnnotationContex
                         </TooltipTrigger>
                     )}
                     {hasSubmit && isActiveLearningMode && (
-                        <TooltipTrigger placement={'bottom'}>
+                        <TooltipTrigger placement={'left'}>
                             <SubmitButton
                                 key={'submit'}
                                 canSubmit={shouldSave}
