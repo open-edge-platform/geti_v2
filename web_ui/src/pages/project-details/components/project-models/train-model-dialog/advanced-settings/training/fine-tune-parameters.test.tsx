b7cda8db99c1389249f35e8584f3a9a842160d4d
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-07-10 10:09:41 +0200

    test: improve tests for fine tune parameters

--- web_ui/src/pages/project-details/components/project-models/train-model-dialog/advanced-settings/training/fine-tune-parameters.test.tsx
+++ web_ui/src/pages/project-details/components/project-models/train-model-dialog/advanced-settings/training/fine-tune-parameters.test.tsx
@@ -1,106 +1,56 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
+import { useState } from 'react';
+
 import { fireEvent, screen } from '@testing-library/react';
 
 import { providersRender as render } from '../../../../../../../test-utils/required-providers-render';
 import { FineTuneParameters } from './fine-tune-parameters.component';
 
 describe('FineTuneParameters', () => {
-    it('Pre-trained weights is selected when trainFromScratch is true', () => {
-        render(
-            <FineTuneParameters
-                trainFromScratch
-                onTrainFromScratchChange={jest.fn()}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={false}
-            />
+    const App = (props: { trainFromScratch: boolean; isReshufflingSubsetsEnabled: boolean }) => {
+        const [trainFromScratch, setTrainFromScratch] = useState(props.trainFromScratch);
+        const [isReshufflingSubsetsEnabled, setIsReshufflingSubsetsEnabled] = useState(
+            props.isReshufflingSubsetsEnabled
         );
 
+        return (
+            <FineTuneParameters
+                trainFromScratch={trainFromScratch}
+                onTrainFromScratchChange={setTrainFromScratch}
+                onReshufflingSubsetsEnabledChange={setIsReshufflingSubsetsEnabled}
+                isReshufflingSubsetsEnabled={isReshufflingSubsetsEnabled}
+            />
+        );
+    };
+
+    it('Fine tune parameter changes based on the trainFromScratch', () => {
+        render(<App trainFromScratch={true} isReshufflingSubsetsEnabled={false} />);
+
+        expect(screen.getByText(/fine\-tune parameters/i)).toBeInTheDocument();
         expect(screen.getByRole('radio', { name: /pre\-trained weights/i })).toBeChecked();
+        expect(screen.getByLabelText('Fine-tune parameters tag')).toHaveTextContent('Pre-trained weights');
         expect(screen.getByRole('radio', { name: /previous training weights/i })).not.toBeChecked();
-    });
 
-    it('Previous training weights is selected when trainFromScratch is false', () => {
-        render(
-            <FineTuneParameters
-                trainFromScratch={false}
-                onTrainFromScratchChange={jest.fn()}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={false}
-            />
-        );
-
+        fireEvent.click(screen.getByRole('radio', { name: /previous training weights/i }));
+        expect(screen.getByLabelText('Fine-tune parameters tag')).toHaveTextContent('Previous training weights');
         expect(screen.getByRole('radio', { name: /previous training weights/i })).toBeChecked();
         expect(screen.getByRole('radio', { name: /pre\-trained weights/i })).not.toBeChecked();
     });
 
-    it('Reshuffle subsets is disabled when Previous training weights is selected', () => {
-        render(
-            <FineTuneParameters
-                trainFromScratch={false}
-                onTrainFromScratchChange={jest.fn()}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={false}
-            />
-        );
+    it('Reshuffle subsets is disabled when Previous training weights is selected, enabled when Pre-trained weights is selected', () => {
+        render(<App trainFromScratch={true} isReshufflingSubsetsEnabled={false} />);
 
-        expect(screen.getByRole('checkbox', { name: /reshuffle subsets/i })).toBeDisabled();
-    });
-
-    it('Reshuffle subsets is enabled when Pre-trained weights is selected', () => {
-        render(
-            <FineTuneParameters
-                trainFromScratch
-                onTrainFromScratchChange={jest.fn()}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={false}
-            />
-        );
-
+        expect(screen.getByRole('radio', { name: /pre\-trained weights/i })).toBeChecked();
         expect(screen.getByRole('checkbox', { name: /reshuffle subsets/i })).toBeEnabled();
-    });
 
-    it('Reshuffle subsets checkbox is checked when isReshufflingSubsetsEnabled is true', () => {
-        render(
-            <FineTuneParameters
-                trainFromScratch
-                onTrainFromScratchChange={jest.fn()}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={true}
-            />
-        );
-
+        fireEvent.click(screen.getByRole('checkbox', { name: /reshuffle subsets/i }));
         expect(screen.getByRole('checkbox', { name: /reshuffle subsets/i })).toBeChecked();
-    });
-
-    it('calls onTrainFromScratchChange with false when Previous training weights gets clicked', () => {
-        const mockedOnTrainFromScratchChange = jest.fn();
-        render(
-            <FineTuneParameters
-                trainFromScratch
-                onTrainFromScratchChange={mockedOnTrainFromScratchChange}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={true}
-            />
-        );
 
         fireEvent.click(screen.getByRole('radio', { name: /previous training weights/i }));
-        expect(mockedOnTrainFromScratchChange).toHaveBeenCalledWith(false);
-    });
 
-    it('calls onTrainFromScratchChange with true when Pre-trained weights gets clicked', () => {
-        const mockedOnTrainFromScratchChange = jest.fn();
-        render(
-            <FineTuneParameters
-                trainFromScratch={false}
-                onTrainFromScratchChange={mockedOnTrainFromScratchChange}
-                onReshufflingSubsetsEnabledChange={jest.fn()}
-                isReshufflingSubsetsEnabled={true}
-            />
-        );
-
-        fireEvent.click(screen.getByRole('radio', { name: /pre\-trained weights/i }));
-        expect(mockedOnTrainFromScratchChange).toHaveBeenCalledWith(true);
+        expect(screen.getByRole('radio', { name: /previous training weights/i })).toBeChecked();
+        expect(screen.getByRole('checkbox', { name: /reshuffle subsets/i })).toBeDisabled();
     });
 });
