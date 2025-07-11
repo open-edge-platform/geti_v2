ff95d815b89bf20682e138b010220fc59463555f
Ma, Xiangxiang <xiangxiang.ma@intel.com>
2025-07-11 18:02:19 +0200

    add revamped configuration endpoint rest api docs

--- interactive_ai/services/director/app/communication/controllers/project_configuration_controller.py
+++ interactive_ai/services/director/app/communication/controllers/project_configuration_controller.py
@@ -35,9 +35,16 @@ class ProjectConfigurationRESTController:
         if isinstance(project_config, NullProjectConfiguration):
             raise ProjectConfigurationNotFoundException(project_identifier.project_id)
         # return single task configuration
-        if len(project_config.task_configs) == 1 or task_id:
-            _task_id = project_config.task_configs[0].task_id or task_id
-            task_config = project_config.get_task_config(_task_id)
+        # use provided task_id if available, check if project is single task
+        if task_id or len(project_config.task_configs) == 1:
+            _task_id = task_id or project_config.task_configs[0].task_id
+            try:
+                task_config = project_config.get_task_config(_task_id)
+            except ValueError:
+                raise ProjectConfigurationNotFoundException(
+                    project_id=project_identifier.project_id,
+                    task_id=ID(_task_id),
+                )
             return ProjectConfigurationRESTViews.task_config_to_rest(task_config)
         # return entire task-chain configuration
         return ProjectConfigurationRESTViews.project_configuration_to_rest(project_config)
