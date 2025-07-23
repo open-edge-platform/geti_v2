997d471ddd6e669c90f6703ba4026e6e61bb963f
Colorado, Camilo <camilo.colorado@intel.com>
2025-07-23 10:33:59 +0200

    add unit tests

--- web_ui/src/pages/annotator/annotation/layers/background-masks.tsx
+++ web_ui/src/pages/annotator/annotation/layers/background-masks.tsx
@@ -4,15 +4,15 @@
 import { Annotation } from '../../../../core/annotations/annotation.interface';
 import { ShapeFactory } from '../shapes/factory.component';
 
-interface BackgroundMasks {
+interface BackgroundMasksProps {
     id: string;
     annotations: Annotation[];
 }
 
-export const BackgroundMasks = ({ id, annotations }: BackgroundMasks) => {
+export const BackgroundMasks = ({ id, annotations }: BackgroundMasksProps) => {
     return (
         <defs xmlns='http://www.w3.org/2000/svg'>
-            <mask id={id}>
+            <mask id={id} aria-label='background-mask'>
                 <rect width='100%' height='100%' fill='white' />
                 <g fill='black' fillOpacity='1'>
                     {annotations.map((annotation) => (
