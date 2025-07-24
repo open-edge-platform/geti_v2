87f8b238d4afbcdafac093e11553dced5d473f9d
Aurélien Adriaenssens <aurelien.adriaenssens@intel.com>
2025-07-24 14:16:15 +0200

    Support background label in Dataset IE (Datumaro)

--- interactive_ai/workflows/common/jobs_common_extras/datumaro_conversion/sc_extractor.py
+++ interactive_ai/workflows/common/jobs_common_extras/datumaro_conversion/sc_extractor.py
@@ -362,10 +362,14 @@ class ScExtractor(dm_DatasetBase):
 
         # check if sc_item has empty label
         has_empty_label = False
+        has_background_label = False
         for sc_ann in sc_annotations:
-            empty_labels = [label for label in sc_ann.get_labels(include_empty=True) if label.is_empty]
-            if len(empty_labels) > 0:
-                has_empty_label = True
+            for label in sc_ann.get_labels(include_empty=True):
+                if label.is_empty:
+                    has_empty_label = True
+                if label.is_background:
+                    has_background_label = True
+            if has_empty_label and has_background_label:
                 break
 
         dm_anns = self._convert_annotations(annotations=sc_annotations, width=width, height=height)
@@ -390,7 +394,10 @@ class ScExtractor(dm_DatasetBase):
             subset=sc_item.subset.name if self._use_subset else DEFAULT_SUBSET_NAME,
             media=dm_media,
             annotations=dm_anns,
-            attributes={"has_empty_label": has_empty_label},
+            attributes={
+                "has_empty_label": has_empty_label,
+                "has_background_label": has_background_label,
+            },
         )
 
     def _save_video(self, video: Video, video_root: str) -> str:
