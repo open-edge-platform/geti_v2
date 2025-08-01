7907d29ac642bd1731b67690acd8fe30c8e30a55
Katarzyna Romanowska <katarzyna.romanowska@intel.com>
2025-08-01 11:44:04 +0200

    remove unnecessary checks

--- web_ui/src/providers/workspaces-provider/use-workspace-identifier.hook.ts
+++ web_ui/src/providers/workspaces-provider/use-workspace-identifier.hook.ts
@@ -4,7 +4,7 @@
 import { useEffect, useMemo } from 'react';
 
 import { WorkspaceIdentifier } from '@geti/core/src/workspaces/services/workspaces.interface';
-import { isEmpty, isEqual } from 'lodash-es';
+import { isEqual } from 'lodash-es';
 import { useParams } from 'react-router-dom';
 
 import { useLastWorkspace } from '../../hooks/use-last-workspace/use-last-workspace.hook';
@@ -15,7 +15,7 @@ export const useWorkspaceIdentifier = (): WorkspaceIdentifier => {
     // Use an empty string so that our unit tests won't fail due to react router not being
     // set up properly
     const { organizationId } = useOrganizationIdentifier();
-    const { workspaceId = '' } = useParams<Pick<WorkspaceIdentifier, 'workspaceId'>>();
+    const { workspaceId } = useParams<Pick<WorkspaceIdentifier, 'workspaceId'>>();
     const { lastWorkspaceId, setLastWorkspaceId } = useLastWorkspace(organizationId, workspaceId);
     const { workspaces } = useWorkspaces();
 
@@ -25,12 +25,10 @@ export const useWorkspaceIdentifier = (): WorkspaceIdentifier => {
         }
     }, [workspaceId, setLastWorkspaceId, lastWorkspaceId]);
 
+    // eslint-disable-next-line max-len
     // For urls lacking workspaceId (workspaceId is empty) we use the lastWorkspaceId if it exists, and if not, we default to the first workspace in the list
-    const resolvedWorkspaceId = !isEmpty(workspaceId)
-        ? workspaceId
-        : !isEmpty(lastWorkspaceId) && lastWorkspaceId !== undefined
-          ? lastWorkspaceId
-          : workspaces[0].id;
+    const resolvedWorkspaceId =
+        workspaceId !== undefined ? workspaceId : lastWorkspaceId !== undefined ? lastWorkspaceId : workspaces[0].id;
 
     return useMemo(() => ({ workspaceId: resolvedWorkspaceId, organizationId }), [resolvedWorkspaceId, organizationId]);
 };
