a0a63f0f7bd56ced99aa580847cb676c36eba209
Katarzyna Romanowska <katarzyna.romanowska@intel.com>
2025-08-07 14:57:37 +0200

    remove unsused variables and imports

--- web_ui/src/core/const.ts
+++ web_ui/src/core/const.ts
@@ -7,7 +7,4 @@ export const IMPORT_DATASET_LEARN_MORE =
 export const DOCS_BASE_URL =
     'https://github.com/open-edge-platform/datumaro/tree/develop/docs/source/docs/data-formats/formats';
 export const CONTACT_SUPPORT = 'https://supporttickets.intel.com/supportrequest?lang=en-US&productId=233385:15548';
-export const TERMS_OF_USE_INTEL = 'https://www.intel.com/content/www/us/en/legal/terms-of-use.html';
-export const PRIVACY_NOTICE = 'https://www.intel.com/content/www/us/en/privacy/intel-privacy-notice.html';
-export const COOKIES_NOTICE = 'https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html';
 export const TERMS_OF_USE_GETI = 'https://docs.geti.intel.com/eula/';
