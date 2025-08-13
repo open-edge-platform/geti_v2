0a471a4b1c155f5a85153e5928f31095cd4f5957
Leonardo Lai <leonardo.lai@intel.com>
2025-08-13 16:06:25 +0200

    fix style

--- interactive_ai/services/director/tests/fixtures/backward_compatibility_configuration.py
+++ interactive_ai/services/director/tests/fixtures/backward_compatibility_configuration.py
@@ -4623,8 +4623,8 @@ def fxt_legacy_model_configuration_rest_view():
                         "description": "Size of each tile in pixels. "
                         "Decreasing the tile size typically results in higher accuracy, "
                         "but it is also more computationally expensive due to the higher number of tiles. "
-                        "In any case, the tile must be large enough to capture the entire object and its surrounding context, "
-                        "so choose a value larger than the size of most annotations.",
+                        "In any case, the tile must be large enough to capture the entire object "
+                        "and its surrounding context, so choose a value larger than the size of most annotations.",
                         "value": 400,
                         "default_value": 128,
                         "min_value": 0,
