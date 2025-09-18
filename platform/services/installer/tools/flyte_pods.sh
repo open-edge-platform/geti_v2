93e154b1936148c898f322f76ac93e33cfa1c50c
Michał Adamczyk <michal1.adamczyk@intel.com>
2025-09-18 09:01:29 +0200

    cleanup

--- platform/services/installer/tools/flyte_pods.sh
+++ platform/services/installer/tools/flyte_pods.sh
@@ -15,9 +15,9 @@ else
   exit 1
 fi
 
-# use provided kubeconfig
-# or taken from KUBECONFIG env var
-# or default to /etc/rancher/k3s/k3s.yaml
+# use kubeconfig from argument if provided
+# otherwise from KUBECONFIG environment variable
+# or fallback to /etc/rancher/k3s/k3s.yaml
 kubeconfig="${2:-${KUBECONFIG:-/etc/rancher/k3s/k3s.yaml}}"
 if [[ ! -f "${kubeconfig}" ]]; then
   echo "Kubeconfig file not found: ${kubeconfig}"
@@ -27,7 +27,7 @@ fi
 # flyte related variables
 FLYTE_SECRET_NAME="flyte-k8s-yaml"
 FLYTE_NAMESPACE="flyte"
-TEMP_K8S_YAML="/tmp/k8s.yaml"
+TEMP_K8S_YAML=$(mktemp -u /tmp/flyte-k8s-yaml.XXXXXX)
 FLYTE_PROPELLER_DEPLOYMENT="flytepropeller"
 
 # jobs related variables
@@ -40,7 +40,7 @@ ENCODED_DATA=$(cat ${TEMP_K8S_YAML} | base64 -w 0)
 kubectl --kubeconfig="${kubeconfig}" patch secret "${FLYTE_SECRET_NAME}" -n "${FLYTE_NAMESPACE}" --type='merge' -p "{\"data\":{\"k8s.yaml\":\"${ENCODED_DATA}\"}}"
 echo ""
 
-echo "Restarting '${FLYTE_PROPELLER_DEPLOYMENT}' to pick up the changes"
+echo "Restarting '${FLYTE_PROPELLER_DEPLOYMENT}' to pick up the changes..."
 kubectl --kubeconfig="${kubeconfig}" rollout restart deployment "${FLYTE_PROPELLER_DEPLOYMENT}" -n "${FLYTE_NAMESPACE}"
 
 # delete all pods in JOBS_NAMESPACE
@@ -51,4 +51,4 @@ if [[ "${delete_flyte_pods}" == "true" ]]; then
 fi
 
 # cleanup
-rm -rf ${TEMP_K8S_YAML}
+rm -rf "${TEMP_K8S_YAML}"
