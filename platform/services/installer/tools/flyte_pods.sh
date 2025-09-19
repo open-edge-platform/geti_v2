4ac090801229d1361ec7070d25f2437bbbc2a3c5
Michał Adamczyk <michal1.adamczyk@intel.com>
2025-09-19 12:04:50 +0200

    update

--- platform/services/installer/tools/flyte_pods.sh
+++ platform/services/installer/tools/flyte_pods.sh
@@ -3,6 +3,17 @@
 # Copyright (C) 2022-2025 Intel Corporation
 # LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
+# flyte related constants
+FLYTE_SECRET_NAME="flyte-k8s-yaml"
+FLYTE_NAMESPACE="flyte"
+TEMP_K8S_YAML=$(mktemp -u /tmp/flyte-k8s-yaml.XXXXXX)
+
+# jobs related constants
+JOBS_NAMESPACE="impt-jobs-production"
+
+# k3s related constants
+K3S_CONFIG="/etc/rancher/k3s/k3s.yaml"
+
 removal_flyte_pods="${1}"
 if [[ "${removal_flyte_pods}" == "enable" ]]; then
   delete_flyte_pods="true"
@@ -18,30 +29,17 @@ fi
 # use kubeconfig from argument if provided
 # otherwise from KUBECONFIG environment variable
 # or fallback to /etc/rancher/k3s/k3s.yaml
-kubeconfig="${2:-${KUBECONFIG:-/etc/rancher/k3s/k3s.yaml}}"
+kubeconfig="${2:-${KUBECONFIG:-"${K3S_CONFIG}"}}"
 if [[ ! -f "${kubeconfig}" ]]; then
   echo "Kubeconfig file not found: ${kubeconfig}"
   exit 1
 fi
 
-# flyte related variables
-FLYTE_SECRET_NAME="flyte-k8s-yaml"
-FLYTE_NAMESPACE="flyte"
-TEMP_K8S_YAML=$(mktemp -u /tmp/flyte-k8s-yaml.XXXXXX)
-FLYTE_PROPELLER_DEPLOYMENT="flytepropeller"
-
-# jobs related variables
-JOBS_NAMESPACE="impt-jobs-production"
-
 echo "Secret '${FLYTE_SECRET_NAME}' will be updated to set delete-resource-on-finalize: ${delete_flyte_pods}"
 kubectl --kubeconfig="${kubeconfig}" get secret "${FLYTE_SECRET_NAME}" -n "${FLYTE_NAMESPACE}" -o jsonpath="{.data.k8s\.yaml}" | base64 --decode > "${TEMP_K8S_YAML}"
 sed -i "s/[[:space:]]*delete-resource-on-finalize: \(true\|false\)/    delete-resource-on-finalize: ${delete_flyte_pods}/g" "${TEMP_K8S_YAML}"
-ENCODED_DATA=$(cat "${TEMP_K8S_YAML}" | base64 -w 0)
+ENCODED_DATA=$(base64 -w 0 "${TEMP_K8S_YAML}")
 kubectl --kubeconfig="${kubeconfig}" patch secret "${FLYTE_SECRET_NAME}" -n "${FLYTE_NAMESPACE}" --type='merge' -p "{\"data\":{\"k8s.yaml\":\"${ENCODED_DATA}\"}}"
-echo ""
-
-echo "Restarting '${FLYTE_PROPELLER_DEPLOYMENT}' to pick up the changes..."
-kubectl --kubeconfig="${kubeconfig}" rollout restart deployment "${FLYTE_PROPELLER_DEPLOYMENT}" -n "${FLYTE_NAMESPACE}"
 
 # delete all pods in JOBS_NAMESPACE
 if [[ "${delete_flyte_pods}" == "true" ]]; then
