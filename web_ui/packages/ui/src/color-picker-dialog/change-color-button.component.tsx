6e6796212dd94e42eb612c29229d24dc956e5483
Plaskiewicz, Paulina <paulina.plaskiewicz@intel.com>
2025-09-23 15:39:34 +0200

    Change way of picking the size of the component

--- web_ui/packages/ui/src/color-picker-dialog/change-color-button.component.tsx
+++ web_ui/packages/ui/src/color-picker-dialog/change-color-button.component.tsx
@@ -1,7 +1,10 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import { ActionButton, View, type BorderRadiusValue, type Responsive } from '../..';
+import { View } from '@adobe/react-spectrum';
+
+import { BorderRadiusValue, Responsive } from '../..';
+import { ActionButton } from '../button/button.component';
 
 interface ChangeColorButtonProps {
     size: 'S' | 'M' | 'L';
@@ -12,12 +15,24 @@ interface ChangeColorButtonProps {
 }
 
 export const ChangeColorButton = ({ size, ariaLabelPrefix, id, color, gridArea }: ChangeColorButtonProps) => {
+    const CHANGE_COLOR_BUTTON_SIZE = {
+        S: {
+            size: 'size-125',
+            margin: 'size-125',
+        },
+        M: {
+            size: 'size-200',
+            margin: 'size-75',
+        },
+        L: {
+            size: 'size-400',
+            margin: 'size-100',
+            radius: 'small',
+        },
+    } as const;
+
     const sizeParameters: { size: string; radius?: Responsive<BorderRadiusValue>; margin: string } =
-        size === 'L'
-            ? { size: 'size-400', radius: 'small', margin: 'size-100' }
-            : size === 'M'
-              ? { size: 'size-200', margin: 'size-75' }
-              : { size: 'size-125', margin: 'size-125' };
+        CHANGE_COLOR_BUTTON_SIZE[size];
 
     return (
         <ActionButton
