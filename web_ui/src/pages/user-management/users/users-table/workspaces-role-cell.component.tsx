a0c35f036aaf2e7382da6833a250e5348bc0e854
Katarzyna Romanowska <katarzyna.romanowska@intel.com>
2025-10-14 13:42:09 +0200

    display workspace roles there with Workspace prefix

--- web_ui/src/pages/user-management/users/users-table/workspaces-role-cell.component.tsx
+++ web_ui/src/pages/user-management/users/users-table/workspaces-role-cell.component.tsx
@@ -17,7 +17,7 @@ interface WorkspacesRoleCellProps extends Omit<TableCellProps, 'cellData'> {
 export const WorkspacesRoleCell = ({ cellData, workspaceId, workspaces, ...rest }: WorkspacesRoleCellProps) => {
     const workspaceRoles = cellData.filter((role) => role.resourceType === RESOURCE_TYPE.WORKSPACE);
 
-    const selectedWorkspaceRoles = capitalize(workspaceRoles.find((role) => role.resourceId === workspaceId)?.role);
+    const selectedWorkspaceRoles = `Workspace ${workspaceRoles.find((role) => role.resourceId === workspaceId)?.role}`;
     const availableWorkspaces = workspaceRoles
         .map((role) => workspaces.find((workspace) => workspace.id === role.resourceId)?.name ?? role.resourceId)
         .join(', ');
