47984d61a38750e56b307e42fe061a142db0b17b
Joao Vilaca <joao.vilaca@intel.com>
2025-11-11 10:50:22 +0100

    Update dataset import link

--- web_ui/src/core/const.ts
+++ web_ui/src/core/const.ts
@@ -3,7 +3,7 @@
 
 /* eslint-disable max-len */
 export const IMPORT_DATASET_LEARN_MORE =
-    'https://github.com/open-edge-platform/datumaro/blob/develop/docs/source/docs/level-up/basic_skills/03_dataset_import_export.rst';
+    'https://github.com/open-edge-platform/datumaro/blob/develop/docs/source/docs/level-up/basic_skills/02_dataset_import_export.rst';
 export const DOCS_BASE_URL =
     'https://github.com/open-edge-platform/datumaro/tree/develop/docs/source/docs/data-formats/formats';
 export const CONTACT_SUPPORT = 'https://supporttickets.intel.com/supportrequest?lang=en-US&productId=233385:15548';
