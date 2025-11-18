f2ac2f2ae06b5980aaee91bd845432931e00ac7a
jcchr <jacek.chrapkowski@intel.com>
2025-11-18 14:26:34 +0100

    allowing definition of k3s node name

--- platform/services/installer/app/commands/install.py
+++ platform/services/installer/app/commands/install.py
@@ -359,7 +359,8 @@ def execute_installation(config: InstallationConfig) -> None:  # noqa: C901, RUF
         logger.info(InstallCmdTexts.k3s_installing)
         try:
             with click_spinner.spinner():
-                install_k3s()
+                node_name = config.node_name.value if config.node_name else None
+                install_k3s(node_name=node_name)
         except K3SInstallationError:
             logger.exception("Error during k3s installation.")
             click.secho(InstallCmdTexts.k3s_installation_failed, fg="red")
@@ -467,7 +468,15 @@ def run_installation_checks(config: InstallationConfig) -> None:
     is_flag=True,
     help="If flag is set - telemetry stack will be installed. By default the stack is not installed.",
 )
-def install(config_file: str | None = None, install_telemetry_stack: bool = False) -> None:
+@click.option(
+    "--node-name",
+    type=str,
+    required=False,
+    help="Name of a node for a k3s cluster installed together with Geti.",
+)
+def install(
+    config_file: str | None = None, install_telemetry_stack: bool = False, node_name: str | None = None
+) -> None:
     """
     Install platform.
     """
@@ -475,7 +484,9 @@ def install(config_file: str | None = None, install_telemetry_stack: bool = Fals
     create_logs_dir()
     configure_logging()
     logger.info(f"{get_target_product_build()} {InstallCmdTexts.start_message}")
-    config = InstallationConfig(interactive_mode=not bool(config_file), install_telemetry_stack=install_telemetry_stack)
+    config = InstallationConfig(
+        interactive_mode=not bool(config_file), install_telemetry_stack=install_telemetry_stack, node_name=node_name
+    )
     run_initial_checks(config=config)
     if config_file:
         load_installation_config_from_yaml(config=config, config_file_path=config_file)
