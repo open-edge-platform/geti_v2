3e1f219528ff4f2b947b54d6c176a233067082d2
Barabanov, Alexander <alexander.barabanov@intel.com>
2025-12-02 13:52:05 +0000

    revert false positives
    Signed-off-by: Barabanov, Alexander <alexander.barabanov@intel.com>

--- platform/services/user_directory/app/endpoints/user_management/activate_user.py
+++ platform/services/user_directory/app/endpoints/user_management/activate_user.py
@@ -58,8 +58,7 @@ async def activate(data: ActivateData) -> PlainTextResponse:  # noqa: D103
                     {"password": data.password, "first_name": data.first_name, "second_name": data.second_name}
                 )
             except ValueError as err:
-                logger.warning(f"User input validation failed: {err}")
-                return PlainTextResponse("Invalid input", status_code=HTTPStatus.BAD_REQUEST)
+                return PlainTextResponse(str(err), status_code=HTTPStatus.BAD_REQUEST)
             handler.check_password_strength(data.password)
         with tracer.start_as_current_span("verify-jwt-token"):
             user = verify_payload(handler, data.token)
--- platform/services/user_directory/app/endpoints/user_management/invite_user.py
+++ platform/services/user_directory/app/endpoints/user_management/invite_user.py
@@ -113,9 +113,8 @@ def invite_user_endpoint(  # noqa: ANN201
                 status_code=HTTPStatus.CONFLICT,
             )
         if error.grpc_status_code == StatusCode.INVALID_ARGUMENT:
-            logger.warning(f"Error on user account invitation: {error}")
             return PlainTextResponse(
-                "Invalid request arguments",
+                str(error),
                 status_code=HTTPStatus.BAD_REQUEST,
             )
         raise
