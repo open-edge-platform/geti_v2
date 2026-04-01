2fcf246298b7afe99c347269bedcb07b32e1edb0
Joao Vilaca <joao.vilaca@intel.com>
2026-04-01 13:25:02 +0200

    Fix eslint/ts issues

--- web_ui/packages/smart-tools/src/segment-anything/pre-processing.ts
+++ web_ui/packages/smart-tools/src/segment-anything/pre-processing.ts
@@ -1,7 +1,7 @@
 // Copyright (C) 2022-2025 Intel Corporation
 // LIMITED EDGE SOFTWARE DISTRIBUTION LICENSE
 
-import * as ort from 'onnxruntime-web';
+import * as ort from 'onnxruntime-common';
 
 import type { OpenCVTypes } from '../opencv/interfaces';
 
